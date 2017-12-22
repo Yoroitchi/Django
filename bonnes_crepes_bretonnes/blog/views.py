aef6014985176001cbc8d4f9920026430f56fe75
Yoro <eleguern@et.esiea.fr>
2017-12-22 16:51:53 +0100

    fin du projet

--- bonnes_crepes_bretonnes/blog/views.py
+++ bonnes_crepes_bretonnes/blog/views.py
@@ -6,36 +6,26 @@ from blog.models import Article
 from .forms import ContactForm, ArticleForm
 
 def accueil(request):
-    """ Afficher tous les articles de notre blog """
+    """ Afficher tous les articles """
     articles = Article.objects.all() # Nous sélectionnons tous nos articles
     return render(request, 'accueil.html', {'derniers_articles': articles})
 
 def sarrasin(request):
-    " Afficher uniquement les crêpes classées dans la Categorie Sarrasin"
+    """ Afficher uniquement les crêpes classées dans la Categorie Sarrasin"""
     articles = Article.objects.filter(categorie__nom__contains="Sarrasin")
     return render(request,'sarrasin.html',{'galettes_sarrasin': articles})
 
 def sucree(request):
-    " Afficher uniquement les crêpes classées dans la Categorie Sucrée"
-    articles = Article.objects.filter(categorie__nom__contains="Sucrée")
+    """" Afficher uniquement les crêpes classées dans la Categorie Sucrée"""
+    articles = Article.objects.filter(categorie__nom__contains="Sucree")
     return render(request,'sucree.html',{'crepes_sucrees': articles})
 
 def lire(request, id):
     article = get_object_or_404(Article, id=id)
     return render(request, 'lire.html', {'article':article})
 
-def today_date(request):
-    return render(request, 'date.html', {'date': datetime.now()})
-
 def contact(request):
-    # Construire le formulaire, soit avec les données postées,
-    # soit vide si l'utilisateur accède pour la première fois
-    # à la page.
-
     form = ContactForm(request.POST or None)
-    # Nous vérifions que les données envoyées sont valides
-    # Cette méthode renvoie False s'il n'y a pas de données
-    # dans le formulaire ou qu'il contient des erreurs.
     if form.is_valid():
         sujet = form.cleaned_data['sujet']
         message = form.cleaned_data['message']
